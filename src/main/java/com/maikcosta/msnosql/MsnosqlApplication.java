23007daa484de36a679c225caadd136d9cbf6d11
Maik Costa <maikcosta@gmail.com>
2025-01-23 22:00:56 -0300

    :tada: Commit inicial

---
+++ src/main/java/com/maikcosta/msnosql/MsnosqlApplication.java
@@ -0,0 +1,13 @@
+package com.maikcosta.msnosql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsnosqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsnosqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maikcosta/msnosql/MsnosqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maikcosta.msnosql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsnosqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
