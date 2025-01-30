ddb75c566539d5b1b9f465af33359cf281f67292
Maik Costa <maikcosta@gmail.com>
2025-01-30 20:27:21 -0300

    :sparkles: feat: Conectando o MongoDB ao repositorio e service

--- src/main/java/com/maikcosta/msnosql/resources/UserResource.java
+++ src/main/java/com/maikcosta/msnosql/resources/UserResource.java
@@ -1,24 +1,32 @@
 package com.maikcosta.msnosql.resources;
 
 import com.maikcosta.msnosql.domain.User;
+import com.maikcosta.msnosql.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping(value="/users")
 
 public class UserResource {
+
+    @Autowired
+    private UserService userService;
+
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
+
+
     @GetMapping
     public ResponseEntity<List<User>> findAll(){
-        User user = new User("1","Maik", "maik@test.com");
-        User user1 = new User("2","Josi", "josi@test.com");
-        List<User> list = new ArrayList<>(Arrays.asList(user, user1));
-        return ResponseEntity.ok().body(list);
+        List<User> users = userService.findAll();
+        logger.debug("Usuários encontrados: " + users);
+        return ResponseEntity.ok().body(users);
     }
 }
---
+++ src/main/java/com/maikcosta/msnosql/services/UserService.java
@@ -0,0 +1,19 @@
+package com.maikcosta.msnosql.services;
+
+import com.maikcosta.msnosql.domain.User;
+import com.maikcosta.msnosql.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+}
