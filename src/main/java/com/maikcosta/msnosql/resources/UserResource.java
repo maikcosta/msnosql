7bbde02951c33313901516d8b729a8e61efbc4f0
Maik Costa <maikcosta@gmail.com>
2025-02-03 16:56:33 -0300

    :sparkles: feat: Delete user via post

--- src/main/java/com/maikcosta/msnosql/resources/UserResource.java
+++ src/main/java/com/maikcosta/msnosql/resources/UserResource.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -50,6 +51,12 @@ public class UserResource {
         user = userService.insert(user);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(userDTO.getId()).toUri();
         return ResponseEntity.created(uri).build();
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<UserDTO> delete(@PathVariable String id){
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
