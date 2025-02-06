91aa73206837e8c99ee96f1f3c991fd5e9610819
Maik Costa <maikcosta@gmail.com>
2025-02-06 14:26:16 -0300

    :sparkles: feat: Vinculando comentários aos posts

--- src/main/java/com/maikcosta/msnosql/config/DatabaseSeeder.java
+++ src/main/java/com/maikcosta/msnosql/config/DatabaseSeeder.java
@@ -3,6 +3,7 @@ package com.maikcosta.msnosql.config;
 import com.maikcosta.msnosql.domain.Post;
 import com.maikcosta.msnosql.domain.User;
 import com.maikcosta.msnosql.dto.AuthorDTO;
+import com.maikcosta.msnosql.dto.CommentDTO;
 import com.maikcosta.msnosql.repository.PostRepository;
 import com.maikcosta.msnosql.repository.UserRepository;
 import org.slf4j.Logger;
@@ -13,8 +14,8 @@ import org.springframework.context.annotation.Configuration;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 import java.util.TimeZone;
 
 @Configuration
@@ -33,7 +34,7 @@ public class DatabaseSeeder {
                 User maria = new User(null, "Maria Souza", "maria@email.com");
 
                 userRepository.saveAll(List.of(joao, maria));
-                logger.info("Usuários inseridos no banco!");
+                logger.info("#Carga inicial - Usuários inseridos no banco!");
             }
 
             User joao = userRepository.findByEmail("joao@email.com").orElse(null);
@@ -48,15 +49,21 @@ public class DatabaseSeeder {
                 try {
                     Post post1 = new Post(null, simpleDateFormat.parse("02/02/2025"), "Título Teste", "Mensagem teste", new AuthorDTO(joao));
                     Post post2 = new Post(null, simpleDateFormat.parse("03/02/2025"), "Título Teste 1", "Mensagem teste 1", new AuthorDTO(maria));
+                    CommentDTO comment1 = new CommentDTO("Comentário 1", simpleDateFormat.parse("03/02/2025"),new AuthorDTO(maria));
+                    CommentDTO comment2 = new CommentDTO("Comentário 2", simpleDateFormat.parse("03/02/2025"),new AuthorDTO(joao));
+                    post1.getComments().addAll(Arrays.asList(comment1));
+                    post2.getComments().addAll(Arrays.asList(comment2));
+                    logger.info("#Carga inicial - Comentários vinculados ao post no banco!");
 
                     postRepository.saveAll(List.of(post1, post2));
-                    logger.info("Posts inseridos no banco!");
+                    logger.info("#Carga inicial - Posts inseridos no banco!");
 
                     joao.setPosts(List.of(post1));
                     maria.setPosts(List.of(post2));
+                    logger.info("#Carga inicial - Posts vinculados ao usuário no banco!");
 
                     userRepository.saveAll(List.of(joao, maria));
-                    logger.info("Usuários atualizados com seus posts!");
+
                 } catch (ParseException e) {
                     logger.error("Erro ao converter data", e);
                 }
--- src/main/java/com/maikcosta/msnosql/domain/Post.java
+++ src/main/java/com/maikcosta/msnosql/domain/Post.java
@@ -1,11 +1,14 @@
 package com.maikcosta.msnosql.domain;
 
 import com.maikcosta.msnosql.dto.AuthorDTO;
+import com.maikcosta.msnosql.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 @Document
 public class Post implements Serializable {
@@ -15,6 +18,9 @@ public class Post implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+
+    private List<CommentDTO> comments = new ArrayList<>();
+
 
     public Post(String id, Date date, String title, String body, AuthorDTO author) {
         this.id = id;
@@ -62,6 +68,14 @@ public class Post implements Serializable {
 
     public void setAuthor(AuthorDTO author) {
         this.author = author;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
     }
 
     @Override
